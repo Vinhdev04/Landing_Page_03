03c18a3a15786a72b64c04a18646b61c02a82690
Vinhdev04 <pcv.fed@gmail.com>
2024-09-03 18:55:12 +0700

    upload source code js JQuery active navbar

--- Education/index.js
+++ Education/index.js
@@ -17,8 +17,6 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
-
-// Hiệu ứng zoom navbar khi scroll body bằng jQuery
 $(document).ready(function () {
   // Hàm xử lý sự kiện cuộn
   $(window).on("scroll", function () {
@@ -28,5 +26,18 @@ $(document).ready(function () {
     } else {
       $("header").removeClass("header-active");
     }
+
+    // Xử lý hiệu ứng active cho menu
+    $("section").each(function () {
+      const top = $(window).scrollTop();
+      const bottom = $(this).offset().top + $(this).outerHeight();
+      const id = $(this).attr("id");
+
+      if (top >= $(this).offset().top - 200 && top < bottom) {
+        $(".navbar ul li a").removeClass("active");
+        $('.navbar ul li a[href="#' + id + '"]').addClass("active");
+        $('.navbar ul li a[href="#' + id + '"]').addClass("active__s");
+      }
+    });
   });
 });
